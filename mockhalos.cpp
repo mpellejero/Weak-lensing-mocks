a39efa1cb8bf9c83b32a62624be59e597b044014
RB Metcalf <bmetcalf@gmail.com>
2016-11-15 13:14:44 +0100

    Changed to BigMultiDark cosmology

--- mockhalos.cpp
+++ mockhalos.cpp
@@ -43,8 +43,9 @@ int main(int arg,char **argv){
   
   InputParams params(paramfile);
 
+  params.print();
   // Make maps from Halo catalogs
-  Lens lens(params,&seed,Planck1yr,false);
+  Lens lens(params,&seed,BigMultiDark,false);
   
   // source redhsifts
   //std::vector<PosType> zss = {2.297,2.119,1.955,1.802,1.66,1.527, 1.403, 1.287, 1.178, 1.075,0.9774, 0.8854, 0.7982, 0.7154,0.6365,0.5612, 0.4892,0.4201, 0.3538,0.2899, 0.2282,0.1686, 0.1108, 0.05465};
